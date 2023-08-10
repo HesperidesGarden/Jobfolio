19823c5a1e544283489bc5ea17d72559cc9f3377
khali <klpham04@gmail.com>
2023-08-10 10:43:56 +0200

    tried to fix capitalization

--- account.py
+++ account.py
@@ -1,11 +1,6 @@
 from flask import Flask, session, render_template, redirect, url_for, request
 from dao.UsersDAO import UsersDAO
 
-# momentan kommt man auf ne placeholder account seite
-# placeholder daten mit db daten ersetzen, bräuchte dafür ein get_user_data oder so
-
-# später die if/else bedingung (auskommetiert unten) wieder rausnehmen damit man als uneingeloggter auf login weitergeleitet wird
-
 def account():
     if 'user_id' in session:
         user_id = session['user_id']
--- portfolio.py
+++ portfolio.py
@@ -3,10 +3,10 @@ from flask import Flask, session, render_template, redirect, url_for, request
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from dao.UsersDAO import UsersDAO
-from dao.educationDAO import EducationDAO
-from dao.languageDAO import LanguageDAO
-from dao.projectDAO import ProjectDAO
-from dao.skillDAO import SkillDAO
+from dao.EducationDAO import EducationDAO
+from dao.LanguageDAO import LanguageDAO
+from dao.ProjectDAO import ProjectDAO
+from dao.SkillDAO import SkillDAO
 from project_form import project_form
 
 def portfolio():
