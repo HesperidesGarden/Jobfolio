34923b0f7768574d40d13f2054d22211cecddc7d
khali <klpham04@gmail.com>
2023-08-14 17:23:53 +0200

    update_user.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from route_methods.portfolio import portfolio
 from route_methods.account import account
 from route_methods.signup import signup_user
 from route_methods.portfolio_edit import portfolio_edit_view
+from route_methods.update_profile import *
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from db import db, create_tables
@@ -107,99 +108,37 @@ def get_signup():
 	return signup_user() 
 
 
-
-# addSkill Route
-@app.route('/edit_skill/', methods=["GET", "POST"])
-def add_skill():
-    user_id = session['user_id']
-    
-    if request.method == 'POST':
-        skill_name = request.form['skill_name']
-        skill_proficiency = request.form['skill_proficiency']
-        skill_description = request.form['skill_description']
-
-        new_skill = Skill(name=skill_name, proficiency=skill_proficiency, description=skill_description, user_id=user_id)
-        db.session.add(new_skill)
-        db.session.commit()
-
-        return redirect(url_for('get_portfolio')) 
-
-    return render_template('portfolio_edit_view.html') 
-
-
-# addLang Route
-@app.route('/edit_lang/', methods=["GET", "POST"])
-def add_lang():
-    user_id = session['user_id']
-    if request.method == 'POST':
-        language_name = request.form['language_name']
-        language_proficiency = request.form['language_proficiency']
-        language_description = request.form['language_description']
-
-        new_language = Language(language_name=language_name, proficiency=language_proficiency, self_evaluation=language_description, user_id=user_id)
-        db.session.add(new_language)
-        db.session.commit()
-
-        return redirect(url_for('get_portfolio'))  
-
-    return render_template('portfolio_edit_view.html')  
-
-# deleteLang
-@app.route('/delete_language/<int:language_id>', methods=['POST'])
-def delete_language(language_id):
-    language = db.session.get(Language, language_id)
-    
-    if language:
-        db.session.delete(language)
-        db.session.commit()
-    else:
-        error_message = 'Error deleting.'
-    return redirect(url_for('get_portfolio'))
-
-# deleteSkill
-@app.route('/delete_skill/<int:skill_id>', methods=['POST'])
-def delete_skill(skill_id):
-    skill = db.session.get(Skill, skill_id)
-
-    if skill:
-        db.session.delete(skill)
-        db.session.commit()
-    else:
-        error_message = 'Error deleting.'
-    return redirect(url_for('get_portfolio'))
-
 # SubmitProject Route
 @app.route("/submit", methods=["POST"])
 def submit_project():
     return redirect("/portfolio/")
 
+# addSkill Route
+@app.route('/edit_skill/', methods=["GET", "POST"])
+def get_add_skill():
+    return add_skill()
+
+
+# addLang Route
+@app.route('/edit_lang/', methods=["GET", "POST"])
+def get_add_lang():
+    return add_lang()
+
+# deleteLang
+@app.route('/delete_language/<int:language_id>', methods=['POST'])
+def get_delete_language(language_id):
+    return delete_language()
+
+# deleteSkill
+@app.route('/delete_skill/<int:skill_id>', methods=['POST'])
+def get_delete_skill(skill_id):
+    return delete_skill()
+
+
 # UpdateProfile Route
 @app.route('/update_user_profile', methods=['POST'])
-def update_user_profile():
-    user_id = session['user_id']
-    user_profile = UserProfile.query.filter_by(user_id=user_id).first()
-
-    profile_picture = request.files['profile_picture']
-    title = request.form.get('user_occupation')  
-    user_description = request.form.get('user_description') 
-    
-    picture_path = None
-    if profile_picture and profile_picture.filename != '' and allowed_file(profile_picture.filename):
-        picture_path = os.path.join('userpictures', profile_picture.filename)
-        profile_picture.save(picture_path)
-
-    if user_profile:
-        user_profile.title = title
-        user_profile.short_description = user_description
-        if picture_path:
-            user_profile.picture = picture_path
-    else:
-        new_user_profile = UserProfile(picture=picture_path, title=title, short_description=user_description, user_id=user_id)
-        db.session.add(new_user_profile)
-
-    db.session.commit()
-
-    return redirect(url_for('get_portfolio')) 
+def get_update_user_profile():
+    update_user_profile()
 
 
 
